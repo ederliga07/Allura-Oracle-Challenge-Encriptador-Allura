00689a24c446c13d74a97004b61300c65cee0aba
Eder Vaca <ederliga@gmail.com>
2023-01-17 20:07:58 -0500

    Agregando ficheros

---
+++ script.js
@@ -0,0 +1,50 @@
+//e --> enter
+//o --> ober
+//i --> imes
+//a --> ai
+//u --> ufat
+
+function encriptar() {
+	var texto = document.getElementById("InputText").value.toLowerCase();
+	//i Para que afecte tanto mayusculas como minusculas
+	//g Para que afecte a toda la linea u oracion
+	//m Para que afecte a multiples lineas o parrafos
+
+	var txtCifrado = texto.replace(/e/igm,"enter");
+	var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+	var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+	var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+	var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+	document.getElementById("imgDer").style.display = "none";
+	document.getElementById("texto").style.display = "none";
+	document.getElementById("texto2").innerHTML = txtCifrado;
+	document.getElementById("btn-copy").style.display = "show";
+	document.getElementById("btn-copy").style.display = "inherit";
+}
+
+function desencriptar() {
+	var texto = document.getElementById("InputText").value.toLowerCase();
+	//i Para que afecte tanto mayusculas como minusculas
+	//g Para que afecte a toda la linea u oracion
+	//m Para que afecte a multiples lineas o parrafos
+
+	var txtCifrado = texto.replace(/enter/igm,"e");
+	var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+	var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+	var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+	var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+	document.getElementById("imgDer").style.display = "none";
+	document.getElementById("texto").style.display = "none";
+	document.getElementById("texto2").innerHTML = txtCifrado;
+	document.getElementById("btn-copy").style.display = "show";
+	document.getElementById("btn-copy").style.display = "inherit";
+}
+
+function copy() {
+	var contenido = document.querySelector("#texto2");
+	contenido.select();
+	document.execCommand("cut");
+	alert("Se copio");
+}
